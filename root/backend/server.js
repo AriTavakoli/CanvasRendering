958a4c94550a67b5162ea52ccfda0c25a23d7857
unknown <aritavakoli98@gmail.com>
2022-12-15 14:30:21 -0800

    folder restructure prep for backend

---
+++ root/backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+// const io = require('socket.io')(server);
+
+
+
+app.use(express.static(path.join(__dirname, '../frontend/client/dist')));
+
+
+
+
+
+// io.on('connect', (socket) => {
+//   console.log('a user connected');
+
+
+//   socket.on('draw', (data) => {
+//     console.log('data', data);
+
+//   });
+
+//   socket.on('sd', (data) => {
+//     console.log('sd', data);
+//     socket.broadcast.emit('sd', data);
+//   });
+
+// });
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
