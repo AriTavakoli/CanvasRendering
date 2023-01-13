9e8266dc1ca0993d0242072dfc8223c1760a16ad
unknown <aritavakoli98@gmail.com>
2023-01-12 23:54:37 -0800

    alias

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -17,9 +17,11 @@ module.exports = {
       '@pages':      path.resolve(__dirname, '../src/components/pages'),
       '@styles':     path.resolve(__dirname, '../src/styles'),
       '@assets':     path.resolve(__dirname, '../src/assets'),
+
+      '@hooks':      path.resolve(__dirname, '../src/hooks'),
       '@hooks':      path.resolve(__dirname, '../src/hooks'),
     }
-  },
+  }
 
   mode: 'development',
 
--- frontend/src/components/layouts/Modal/Modal.jsx
+++ frontend/src/components/layouts/Modal/Modal.jsx
@@ -31,9 +31,6 @@ export default function Modal({ }) {
   })
 
 
-
-
-
   return (
     <>
       <div className='modalButton'>
